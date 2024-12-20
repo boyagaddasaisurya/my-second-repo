5b564e361a32eaed9159aac7c547dc9b2dbb0da2
boyagaddasaisurya <boyavalmiki30@gmail.com>
2024-12-20 12:10:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmpSal from './Components/empsal'
+import ShowEmpData from './Components/ShowEmpData'
+//import Product from './Components/Product'
+//import EmpSal from './Components/EmpSal'
+export default function App() {
+  return (
+    <div>
+      <EmpSal/>
+
+    </div>
+  )
+}
